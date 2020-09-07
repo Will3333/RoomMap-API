b6b5a005bef7d0e837217cc0a78aa49e73758b74
William Smith <william.smith@wsmi.pro>
2020-09-07 22:40:42 +0200

    Tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ version = "0.1"
 
 val COROUTINES_VERSION = "1.3.9"
 val SERIALIZATION_VERSION = "1.0.0-RC"
+val KAML_VERSION = "0.21.0"
 val CLIKT_VERSION = "3.0.0"
 val KWSMILIB_VERSION = "0.5.0"
 val KTOR_VERSION = "1.4.0"
@@ -22,6 +23,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$COROUTINES_VERSION")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$SERIALIZATION_VERSION")
+    implementation("com.charleskorn.kaml:kaml:$KAML_VERSION")
     implementation("com.github.ajalt.clikt:clikt:$CLIKT_VERSION")
     implementation("pro.wsmi:kwsmilib-jvm:$KWSMILIB_VERSION")
     implementation("io.ktor:ktor-client-core:$KTOR_VERSION")
--- src/main/kotlin/com/la2soft/roommap/backend/Backend.kt
+++ src/main/kotlin/com/la2soft/roommap/backend/Backend.kt
@@ -1,10 +1,14 @@
 package com.la2soft.roommap.backend
 
+import com.charleskorn.kaml.Yaml
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.option
+import com.la2soft.roommap.backend.config.BackendConfiguration
+import kotlinx.serialization.ExperimentalSerializationApi
 import pro.wsmi.kwsmilib.jvm.io.checkFileExistsAndExitIfNot
 import pro.wsmi.kwsmilib.jvm.io.checkIsReadableAndExitIfNot
 import java.io.File
+import kotlin.system.exitProcess
 
 const val APP_NAME = "RoomMap"
 const val APP_VERSION = "0.1"
@@ -15,6 +19,7 @@ class BasicLineCmd : CliktCommand()
 {
     val cfgFilePathCLA: String? by option("-f", "--config-file", help = "Path of the backend configuration file")
 
+    @ExperimentalSerializationApi
     override fun run()
     {
         val configFile = if (!this.cfgFilePathCLA.isNullOrBlank())
@@ -23,6 +28,18 @@ class BasicLineCmd : CliktCommand()
             File(DEFAULT_CFG_FILE_DIR, DEFAULT_CFG_FILE_NAME)
         configFile.checkFileExistsAndExitIfNot("The configuration file ${configFile.canonicalFile} does not exist.", 1)
         configFile.checkIsReadableAndExitIfNot("The configuration file ${configFile.canonicalFile} is not readable.", 2)
+
+        val backendCfg = try {
+            Yaml.default.decodeFromString(BackendConfiguration.serializer(), configFile.readText(Charsets.UTF_8))
+        } catch (e: Exception) {
+            println("There is an error in the configuration file ${configFile.canonicalFile}.")
+            println(e.localizedMessage)
+            exitProcess(3)
+        }
+
+        println(backendCfg)
+
+        println(backendCfg.proxy?.hostString)
     }
 }
 
