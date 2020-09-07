b399fc19f29b914b2dd71d4750fec28d7999d9dc
William Smith <william.smith@wsmi.pro>
2020-09-07 13:56:17 +0200

    - Adding http 200 response to a supported specification versions request

---
+++ src/main/kotlin/com/la2soft/roommap/backend/matrix/api/SupportedSpecVersionReq200Response.kt
@@ -0,0 +1,11 @@
+package com.la2soft.roommap.backend.matrix.api
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SupportedSpecVersionReq200Response (
+        val versions : List<String>,
+        @SerialName("unstable_features")
+        val unstableFeatures : Map<String, Boolean>? = null
+)
