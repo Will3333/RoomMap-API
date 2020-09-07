5d3948faf39f4dfa172af4745246ed7ba26bc577
William Smith <william.smith@wsmi.pro>
2020-09-07 13:33:20 +0200

    Base of project

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "1.4.0"
+    kotlin("plugin.serialization") version "1.4.0"
+}
+
+group = "com.la2soft"
+version = "0.1"
+
+val COROUTINES_VERSION = "1.3.9"
+val SERIALIZATION_VERSION = "1.0.0-RC"
+val KWSMILIB_VERSION = "0.5.0"
+val KTOR_VERSION = "1.4.0"
+
+repositories {
+    mavenCentral()
+    jcenter()
+    mavenLocal()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$COROUTINES_VERSION")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$SERIALIZATION_VERSION")
+    implementation("pro.wsmi:kwsmilib-jvm:$KWSMILIB_VERSION")
+    implementation("io.ktor:ktor-client-core:$KTOR_VERSION")
+    implementation("io.ktor:ktor-client-serialization-jvm:$KTOR_VERSION")
+    implementation("io.ktor:ktor-client-apache:$KTOR_VERSION")
+}
