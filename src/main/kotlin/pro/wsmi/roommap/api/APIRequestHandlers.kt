9dac1ab90953789d2a75a5324dd3657d9bc54ea9
William Smith <william.smith@wsmi.pro>
2020-10-11 22:08:24 +0200

    Adding of serialization checking

--- src/main/kotlin/pro/wsmi/roommap/api/APIRequestHandlers.kt
+++ src/main/kotlin/pro/wsmi/roommap/api/APIRequestHandlers.kt
@@ -1,6 +1,7 @@
 package pro.wsmi.roommap.api
 
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
 import org.http4k.core.HttpHandler
 import org.http4k.core.Request
@@ -16,85 +17,104 @@ fun handleAPIRoomListReq(debugMode: Boolean, matrixServers: List<MatrixServer>) 
         prettyPrint = debugMode
     }
 
-    val roomListReq = jsonSerializer.decodeFromString(APIRoomListReq.serializer(), req.bodyString())
-
-    val apiRoomList = mutableListOf<MatrixRoom>()
-    matrixServers.forEach { server ->
-        apiRoomList.addAll(
-            server.matrixRooms.map {room ->
-                MatrixRoom(room.roomId, server.id.toString(), room.aliases, room.canonicalAlias, room.name, room.numJoinedMembers, room.topic, room.worldReadable, room.guestCanJoin, room.avatarUrl)
-            }
-        )
+    val roomListReq = try {
+        jsonSerializer.decodeFromString(APIRoomListReq.serializer(), req.bodyString())
+    } catch (e: SerializationException) {
+        null
     }
 
-    val filteredRoomList = when(roomListReq.sortingElement)
+    if (roomListReq != null)
     {
-        MatrixRoomListSortingElement.NUM_JOINED_MEMBERS -> {
-            if (roomListReq.descendingSort)
-                apiRoomList.sortedByDescending { it.numJoinedMembers }
-            else
-                apiRoomList.sortedBy { it.numJoinedMembers }
+        val apiRoomList = mutableListOf<MatrixRoom>()
+        matrixServers.forEach { server ->
+            apiRoomList.addAll(
+                    server.matrixRooms.map {room ->
+                        MatrixRoom(room.roomId, server.id.toString(), room.aliases, room.canonicalAlias, room.name, room.numJoinedMembers, room.topic, room.worldReadable, room.guestCanJoin, room.avatarUrl)
+                    }
+            )
         }
-        MatrixRoomListSortingElement.ROOM_NAME -> {
-            if (roomListReq.descendingSort)
-                apiRoomList.sortedByDescending { it.name }
-            else
-                apiRoomList.sortedBy { it.name }
-        }
-        MatrixRoomListSortingElement.SERVER_NAME -> {
-            val serversById = matrixServers.associateBy { it.id.toString() }
-            if (roomListReq.descendingSort)
-                apiRoomList.sortedByDescending { serversById[it.serverId]?.name }
-            else
-                apiRoomList.sortedBy { serversById[it.serverId]?.name }
-        }
-    }.let {
-        if (roomListReq.filteringNumJoinedMembersGTE != null)
-            it.filter { matrixRoom -> matrixRoom.numJoinedMembers >= roomListReq.filteringNumJoinedMembersGTE!! }
-        else
-            it
-    }.let {
-        if (roomListReq.filteringNumJoinedMembersLTE != null)
-            it.filter { matrixRoom -> matrixRoom.numJoinedMembers <= roomListReq.filteringNumJoinedMembersLTE!! }
-        else
-            it
-    }.let {
-        when(roomListReq.filteringGuestCanJoin)
+
+        val filteredRoomList = when(roomListReq.sortingElement)
         {
-            MatrixRoomGuestCanJoinFilter.NO_FILTER -> it
-            MatrixRoomGuestCanJoinFilter.CAN_JOIN -> it.filter { matrixRoom ->
-                matrixRoom.guestCanJoin
+            MatrixRoomListSortingElement.NUM_JOINED_MEMBERS -> {
+                if (roomListReq.descendingSort)
+                    apiRoomList.sortedByDescending { it.numJoinedMembers }
+                else
+                    apiRoomList.sortedBy { it.numJoinedMembers }
             }
-            MatrixRoomGuestCanJoinFilter.CAN_NOT_JOIN -> it.filterNot {matrixRoom ->
-                matrixRoom.guestCanJoin
+            MatrixRoomListSortingElement.ROOM_NAME -> {
+                if (roomListReq.descendingSort)
+                    apiRoomList.sortedByDescending { it.name }
+                else
+                    apiRoomList.sortedBy { it.name }
+            }
+            MatrixRoomListSortingElement.SERVER_NAME -> {
+                val serversById = matrixServers.associateBy { it.id.toString() }
+                if (roomListReq.descendingSort)
+                    apiRoomList.sortedByDescending { serversById[it.serverId]?.name }
+                else
+                    apiRoomList.sortedBy { serversById[it.serverId]?.name }
+            }
+        }.let {
+            if (roomListReq.filteringNumJoinedMembersGTE != null)
+                it.filter { matrixRoom -> matrixRoom.numJoinedMembers >= roomListReq.filteringNumJoinedMembersGTE!! }
+            else
+                it
+        }.let {
+            if (roomListReq.filteringNumJoinedMembersLTE != null)
+                it.filter { matrixRoom -> matrixRoom.numJoinedMembers <= roomListReq.filteringNumJoinedMembersLTE!! }
+            else
+                it
+        }.let {
+            when(roomListReq.filteringGuestCanJoin)
+            {
+                MatrixRoomGuestCanJoinFilter.NO_FILTER -> it
+                MatrixRoomGuestCanJoinFilter.CAN_JOIN -> it.filter { matrixRoom ->
+                    matrixRoom.guestCanJoin
+                }
+                MatrixRoomGuestCanJoinFilter.CAN_NOT_JOIN -> it.filterNot {matrixRoom ->
+                    matrixRoom.guestCanJoin
+                }
+            }
+        }.let {
+            when(roomListReq.filteringWorldReadable)
+            {
+                MatrixRoomWorldReadableFilter.NO_FILTER -> it
+                MatrixRoomWorldReadableFilter.IS_WORLD_READABLE -> it.filter { matrixRoom ->
+                    matrixRoom.worldReadable
+                }
+                MatrixRoomWorldReadableFilter.IS_NOT_WORLD_READABLE -> it.filterNot { matrixRoom ->
+                    matrixRoom.worldReadable
+                }
             }
         }
-    }.let {
-        when(roomListReq.filteringWorldReadable)
+
+        val roomsTotalNum = filteredRoomList.size
+
+        val slicedAPIRoomList = if (roomListReq.end != null)
+            filteredRoomList.slice(IntRange(roomListReq.start, if (roomListReq.end!! < (roomsTotalNum-1)) roomListReq.end!! else roomsTotalNum-1))
+        else
+            filteredRoomList
+
+        if (slicedAPIRoomList.isNotEmpty())
         {
-            MatrixRoomWorldReadableFilter.NO_FILTER -> it
-            MatrixRoomWorldReadableFilter.IS_WORLD_READABLE -> it.filter { matrixRoom ->
-                matrixRoom.worldReadable
+            val apiRoomListReqResponse = APIRoomListReqResponse(slicedAPIRoomList.toList(), roomsTotalNum)
+
+            val responseBodyStr = try {
+                jsonSerializer.encodeToString(APIRoomListReqResponse.serializer(), apiRoomListReqResponse)
+            } catch (e: SerializationException) {
+                null
             }
-            MatrixRoomWorldReadableFilter.IS_NOT_WORLD_READABLE -> it.filterNot { matrixRoom ->
-                matrixRoom.worldReadable
-            }
+
+            if (responseBodyStr != null)
+                Response(Status.OK).body(responseBodyStr)
+            else
+                Response(Status.INTERNAL_SERVER_ERROR)
         }
+        else Response(Status.NOT_FOUND)
     }
-
-    val roomsTotalNum = filteredRoomList.size
-
-    val slicedAPIRoomList = if (roomListReq.end != null)
-        filteredRoomList.slice(IntRange(roomListReq.start, if (roomListReq.end!! < (roomsTotalNum-1)) roomListReq.end!! else roomsTotalNum-1))
     else
-        filteredRoomList
-
-    if (slicedAPIRoomList.isNotEmpty())
-    {
-        val apiRoomListReqResponse = APIRoomListReqResponse(slicedAPIRoomList.toList(), roomsTotalNum)
-        Response(Status.OK).body(jsonSerializer.encodeToString(APIRoomListReqResponse.serializer(), apiRoomListReqResponse))
-    }
-    else Response(Status.NOT_FOUND)
+        Response(Status.NOT_FOUND)
 }
 
 @ExperimentalSerializationApi
@@ -115,7 +135,16 @@ fun handleAPIServerListReq(debugMode: Boolean, matrixServers: List<MatrixServer>
 
     val apiServerListReqResponse = APIServerListReqResponse(serverList)
 
-    Response(Status.OK).body(jsonSerializer.encodeToString(APIServerListReqResponse.serializer(), apiServerListReqResponse))
+    val responseBodyStr = try {
+        jsonSerializer.encodeToString(APIServerListReqResponse.serializer(), apiServerListReqResponse)
+    } catch (e : SerializationException) {
+        null
+    }
+
+    if (responseBodyStr != null)
+        Response(Status.OK).body(responseBodyStr)
+    else
+        Response(Status.INTERNAL_SERVER_ERROR)
 }
 
 @ExperimentalSerializationApi
@@ -125,22 +154,41 @@ fun handleAPIServerReq(debugMode: Boolean, matrixServers: List<MatrixServer>) : 
         prettyPrint = debugMode
     }
 
-    val serverReq = jsonSerializer.decodeFromString(APIServerReq.serializer(), req.bodyString())
-
-    var foundServer : MatrixServer? = null
-    for (server in matrixServers)
-    {
-        if (server.id.toString() == serverReq.serverId)
-        {
-            foundServer = server
-            break
-        }
+    val serverReq = try {
+        jsonSerializer.decodeFromString(APIServerReq.serializer(), req.bodyString())
+    } catch (e: SerializationException) {
+        null
     }
 
-    if (foundServer != null)
+    if (serverReq != null)
     {
-        val apiServerReqResponse = APIServerReqResponse(foundServer.id.toString(), pro.wsmi.roommap.lib.api.MatrixServer(foundServer.name, foundServer.apiURL, foundServer.updateFreq))
-        Response(Status.OK).body(jsonSerializer.encodeToString(APIServerReqResponse.serializer(), apiServerReqResponse))
+        var foundServer : MatrixServer? = null
+        for (server in matrixServers)
+        {
+            if (server.id.toString() == serverReq.serverId)
+            {
+                foundServer = server
+                break
+            }
+        }
+
+        if (foundServer != null)
+        {
+            val apiServerReqResponse = APIServerReqResponse(foundServer.id.toString(), MatrixServer(foundServer.name, foundServer.apiURL, foundServer.updateFreq))
+
+            val responseBodyStr = try {
+                jsonSerializer.encodeToString(APIServerReqResponse.serializer(), apiServerReqResponse)
+            } catch (e: SerializationException) {
+                null
+            }
+
+            if (responseBodyStr != null)
+                Response(Status.OK).body(responseBodyStr)
+            else
+                Response(Status.INTERNAL_SERVER_ERROR)
+        }
+        else
+            Response(Status.NOT_FOUND)
     }
     else
         Response(Status.NOT_FOUND)
