b0484f2ffc9eaa423e6135006d14287a832005f7
William Smith <william.smith@wsmi.pro>
2020-10-12 19:35:15 +0200

    upgrade to kotlinx serialization 1.0.0 and kwsmilib 0.10.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,10 +11,10 @@ version = "0.0.1"
 
 val ROOMMAP_LIB_VERSION = "0.1.0"
 val COROUTINES_VERSION = "1.3.9"
-val SERIALIZATION_VERSION = "1.0.0-RC"
+val SERIALIZATION_VERSION = "1.0.0"
 val KAML_VERSION = "0.21.0"
 val CLIKT_VERSION = "3.0.0"
-val KWSMILIB_VERSION = "0.8.3"
+val KWSMILIB_VERSION = "0.10.0"
 val KMONGO_VERSION= "4.1.2"
 val HTTP4K_VERSION = "3.260.0"
 
@@ -29,7 +29,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("pro.wsmi:roommap-lib-jvm:$ROOMMAP_LIB_VERSION")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$COROUTINES_VERSION")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$SERIALIZATION_VERSION")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$SERIALIZATION_VERSION")
     implementation("com.charleskorn.kaml:kaml:$KAML_VERSION")
     implementation("com.github.ajalt.clikt:clikt:$CLIKT_VERSION")
     implementation("pro.wsmi:kwsmilib-jvm:$KWSMILIB_VERSION")
